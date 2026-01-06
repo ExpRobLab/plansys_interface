fd0016172102d7f5f097e02b424289991e2194a0
xxxxxChristian02xxxxx <negcris02@gmail.com>
2026-01-06 18:49:39 +0100

    working

--- src/move_to_photograph_action_node.cpp
+++ src/move_to_photograph_action_node.cpp
@@ -4,7 +4,7 @@
 #include <fstream>
 #include <vector>
 #include <filesystem>
-
+#include <algorithm>
 #include "plansys2_executor/ActionExecutorClient.hpp"
 #include "rclcpp/rclcpp.hpp"
 #include "rclcpp_action/rclcpp_action.hpp"
@@ -25,6 +25,7 @@
 #include <tf2_geometry_msgs/tf2_geometry_msgs.hpp>
 using namespace std::chrono_literals;
 namespace fs = std::filesystem;
+// states of the robot
 enum class AlignState
 {
     NAVIGATE,
@@ -33,6 +34,37 @@ enum class AlignState
 class MoveToPhotograph : public plansys2::ActionExecutorClient
 {
 public:
+    MoveToPhotograph() : plansys2::ActionExecutorClient("move_to_photograph", 500ms)
+    {
+
+        // PUBLISHERS
+        cmd_vel_pub_ = this->create_publisher<geometry_msgs::msg::Twist>(
+
+            "/cmd_vel", 10);
+        img_pub_ = this->create_publisher<sensor_msgs::msg::Image>("images_detected", 10);
+
+        // SUBSCRIBERS
+        odom_sub_ = this->create_subscription<nav_msgs::msg::Odometry>(
+            "/odom", 10, std::bind(&MoveToPhotograph::odom_callback, this, std::placeholders::_1));
+        auto qos = rclcpp::SensorDataQoS();
+        img_sub_ = this->create_subscription<sensor_msgs::msg::Image>(
+            "/camera/image", qos,
+            [this](const sensor_msgs::msg::Image::SharedPtr msg)
+            { last_img_ = msg; });
+        aruco_sub_ = this->create_subscription<aruco_opencv_msgs::msg::ArucoDetection>(
+            "/aruco_detections", 10, std::bind(&MoveToPhotograph::detection_callback, this, std::placeholders::_1));
+
+        // CLIENTS
+        nav2_client_ = rclcpp_action::create_client<nav2_msgs::action::NavigateToPose>(
+            this, "navigate_to_pose");
+
+        // attributes
+        nav2_done_ = false;
+        nav2_success_ = false;
+        tf_buffer_ = std::make_shared<tf2_ros::Buffer>(this->get_clock());
+        tf_listener_ = std::make_shared<tf2_ros::TransformListener>(*tf_buffer_);
+    }
+
     struct MarkerData
     {
         long id;
@@ -44,71 +76,49 @@ public:
         double goal_y;
     };
 
-    MoveToPhotograph() : plansys2::ActionExecutorClient("move_to_photograph_mode", 500ms)
+private:
+    // function to update the current coordinated of the target wrt the robot
+   void detection_callback(const aruco_opencv_msgs::msg::ArucoDetection::SharedPtr msg)
+{
+    for (const auto &marker : msg->markers)
     {
+        // checking is the marker seen is the list of the targt marker
+        bool is_in_list = std::any_of(targets_list.begin(), targets_list.end(),
+            [&marker](const MarkerData &target) {
+                return target.id == (long)marker.marker_id;
+            });
 
-        // 
-        odom_sub_ = this->create_subscription<nav_msgs::msg::Odometry>(
-            "/odom", 10,
-            std::bind(&MoveToPhotograph::odom_callback, this, std::placeholders::_1));
-        cmd_vel_pub_ = this->create_publisher<geometry_msgs::msg::Twist>(
-            "/cmd_vel", 10);
-
-        nav2_client_ = rclcpp_action::create_client<nav2_msgs::action::NavigateToPose>(
-            this, "navigate_to_pose");
-
-        auto qos = rclcpp::SensorDataQoS();
-        img_sub_ = this->create_subscription<sensor_msgs::msg::Image>(
-            "/camera/image", qos,
-            [this](const sensor_msgs::msg::Image::SharedPtr msg)
-            { last_img_ = msg; });
-
-        img_pub_ = this->create_publisher<sensor_msgs::msg::Image>("images_detected",10);
-
-        aruco_sub_ = this->create_subscription<aruco_opencv_msgs::msg::ArucoDetection>(
-            "/aruco_detections", 10, std::bind(&MoveToPhotograph::detection_callback, this, std::placeholders::_1));
-
-        nav2_done_ = false;
-        nav2_success_ = false;
-        tf_buffer_ = std::make_shared<tf2_ros::Buffer>(this->get_clock());
-        tf_listener_ = std::make_shared<tf2_ros::TransformListener>(*tf_buffer_);
-    }
-    void detection_callback(const aruco_opencv_msgs::msg::ArucoDetection::SharedPtr msg)
-    {
-        for (const auto &marker : msg->markers)
+        if (!is_in_list)
         {
-            if (marker.marker_id != marker_target.id)
-                continue; //  SOLO il marker target
-              
-            try
-            {
-                std::string marker_frame = "marker_" + std::to_string(marker.marker_id);
+            continue;
+        }
 
-                // TRANSFORM LIVE: base_link <- marker
-                auto tf = tf_buffer_->lookupTransform(
-                    "base_link",
-                    marker_frame,
-                    tf2::TimePointZero);
+        try
+        {
+            std::string marker_frame = "marker_" + std::to_string(marker.marker_id);
 
-                live_marker_x_ = tf.transform.translation.x;
-                live_marker_y_ = tf.transform.translation.y;
-                if (marker_visible_ == false)
-                    marker_visible_ = true;
-            }
-            catch (const tf2::TransformException &)
-            {
-                // ERROR
-                // marker_visible_ = false;
-            }
+            // TRANSFORM LIVE: base_link <- marker
+            auto tf = tf_buffer_->lookupTransform(
+                "base_link",
+                marker_frame,
+                tf2::TimePointZero);
+            this->live_marker_x_ = tf.transform.translation.x;
+            this->live_marker_y_ = tf.transform.translation.y;
+            this->marker_visible_ = true;
+        }
+        catch (const tf2::TransformException &ex)
+        {
+            RCLCPP_WARN(get_logger(), "TF failed for ID %d: %s", marker.marker_id, ex.what());
+            this->marker_visible_ = false;
         }
     }
-
-private:
+}
+    // function to obtain the ws path
     std::string get_ws_path()
     {
         try
         {
-            std::string pkg_path = ament_index_cpp::get_package_share_directory("bme_gazebo_basics");
+            std::string pkg_path = ament_index_cpp::get_package_share_directory("plansys_interface");
             fs::path p(pkg_path);
             return p.parent_path().parent_path().parent_path().parent_path().string();
         }
@@ -118,10 +128,11 @@ private:
             return ".";
         }
     }
+    // function to get the markers seen and take the first to go to wp
     void load_detected_markers()
     {
-        std::string ws_path = get_ws_path();
-        std::string file_path = ws_path + "/points_detected/detected_markers.yaml";
+        // get the path of the yaml file
+        std::string file_path = get_ws_path() + "/points_detected/detected_markers.yaml";
         std::ifstream infile(file_path);
 
         if (!infile.is_open())
@@ -130,12 +141,13 @@ private:
             return;
         }
 
+        std::vector<MarkerData> all_markers;
         std::string line;
         MarkerData m;
         int fields_found = 0;
-        bool marker_loaded = false;
 
-        while (std::getline(infile, line) && !marker_loaded)
+        // get the markers
+        while (std::getline(infile, line))
         {
             size_t first = line.find_first_not_of(" \t-");
             if (first == std::string::npos)
@@ -156,7 +168,6 @@ private:
             else if (clean_line.find("frame:") == 0)
             {
                 m.frame = clean_line.substr(6);
-                m.frame.erase(0, m.frame.find_first_not_of(" "));
                 fields_found++;
             }
             else if (clean_line.find("goal_x:") == 0)
@@ -182,26 +193,49 @@ private:
 
             if (fields_found == 7)
             {
-                this->marker_target = m;
-                marker_loaded = true;
+                all_markers.push_back(m);
+                fields_found = 0;
             }
         }
         infile.close();
+
+        if (all_markers.empty())
+            return;
+
+        // get first marker name
+        std::string target_name = all_markers[0].name;
+        this->targets_list.clear();
+
+        // take only the markers with the same name
+        for (const auto &marker : all_markers)
+        {
+            if (marker.name == target_name)
+            {
+                this->targets_list.push_back(marker);
+            }
+        }
+        if (!targets_list.empty())
+        {
+            this->marker_target = targets_list[0];
+            RCLCPP_INFO(get_logger(), "loaded %zu marker with name: %s", targets_list.size(), target_name.c_str());
+        }
     }
-
-    void remove_first_marker_from_yaml()
+    // function to remove the markers with the same name as the processed one
+    void remove_from_yaml()
     {
-        std::string ws_path = get_ws_path();
-        std::string file_path = ws_path + "/points_detected/detected_markers.yaml";
-        std::vector<MarkerData> remaining;
+        // get path
+        std::string file_path = get_ws_path() + "/points_detected/detected_markers.yaml";
+        std::vector<MarkerData> remaining_on_disk;
 
+        std::string name_to_remove = this->marker_target.name;
+        // get markers from yaml file
         std::ifstream infile(file_path);
         if (infile.is_open())
         {
             std::string line;
             MarkerData m;
             int fields = 0;
-            bool skipped = false;
+
             while (std::getline(infile, line))
             {
                 size_t first = line.find_first_not_of(" \t-");
@@ -223,7 +257,6 @@ private:
                 else if (clean.find("frame:") == 0)
                 {
                     m.frame = clean.substr(6);
-                    m.frame.erase(0, m.frame.find_first_not_of(" "));
                     fields++;
                 }
                 else if (clean.find("goal_x:") == 0)
@@ -247,36 +280,44 @@ private:
                     fields++;
                 }
 
-                if (fields == 7)
+                if (fields == 7) // removing markers with same name as target
                 {
-                    if (!skipped)
-                        skipped = true; // Salta il primo
-                    else
-                        remaining.push_back(m);
+                    if (m.name != name_to_remove)
+                    {
+                        remaining_on_disk.push_back(m);
+                    }
+
                     fields = 0;
                 }
             }
             infile.close();
         }
 
+        // writing the markers
         std::ofstream outfile(file_path, std::ios::trunc);
         if (outfile.is_open())
         {
             outfile << "markers:\n";
-            for (const auto &rm : remaining)
+            for (const auto &rm : remaining_on_disk)
             {
-                outfile << "  - id: " << rm.id << "\n    name: " << rm.name << "\n    frame: " << rm.frame << "\n    x: " << rm.x << "\n    y: " << rm.y << "\n    goal_x: " << rm.goal_x << "\n    goal_y: " << rm.goal_y << "\n";
+                outfile << "  - id: " << rm.id << "\n"
+                        << "    name: " << rm.name << "\n"
+                        << "    frame: " << rm.frame << "\n"
+                        << "    x: " << rm.x << "\n"
+                        << "    y: " << rm.y << "\n"
+                        << "    goal_x: " << rm.goal_x << "\n"
+                        << "    goal_y: " << rm.goal_y << "\n";
             }
             outfile.close();
         }
     }
-
+    // do work for the plansys
     void do_work() override
     {
         auto args = get_arguments();
         if (args.size() < 2)
         {
-            finish(false, 0.0, "Argomenti insufficienti");
+            finish(false, 0.0, "no sufficient arguments");
             return;
         }
 
@@ -284,15 +325,15 @@ private:
 
         if (marker_target.name.empty())
         {
-            finish(false, 0.0, "Nessun marker disponibile");
+            finish(false, 0.0, "no amrker avaiable");
             return;
         }
 
         std::string marker_to = "home_point";
         double gx = marker_target.goal_x;
         double gy = marker_target.goal_y;
-        if (this->state_ == AlignState::NAVIGATE)
-        { // invio goal solo la prima volta
+        if (this->state_ == AlignState::NAVIGATE) // navigate to wp
+        {
             if (!goal_sent_)
             {
                 if (!nav2_client_->wait_for_action_server(1s))
@@ -331,6 +372,7 @@ private:
                     }
                     else
                     {
+                        // wp reached
                         this->nav2_done_ = true;
                         this->nav2_success_ = true;
                         this->state_ = AlignState::ALIGN;
@@ -341,94 +383,90 @@ private:
                 goal_sent_ = true;
                 this->start_x_ = this->current_x_;
                 this->start_y_ = this->current_y_;
-            } // Calcolo distanza e progresso
+            }
+            // feedback update
             double total_dist = std::hypot(gx - this->start_x_, gy - this->start_y_);
             double rem_dist = std::hypot(gx - current_x_, gy - current_y_);
             progress_ = total_dist > 0.0 ? 1.0 - std::min(rem_dist / total_dist, 1.0) : 1.0;
             send_feedback(progress_, "Moving to " + marker_to);
         }
-
-        // Threshold per completamento automatico
-        else if (this->state_ == AlignState::ALIGN)
+        else if (this->state_ == AlignState::ALIGN) // aligning state
         {
-            RCLCPP_INFO(get_logger(), "align state , roating for the alignement");
-            if (!marker_visible_)
+            if (!this->marker_visible_) // if no marker was seen
             {
+                geometry_msgs::msg::Twist cmd;
+                cmd.angular.z = 0.5;
+                cmd_vel_pub_->publish(cmd);
                 send_feedback(progress_, "Waiting for marker...");
             }
             else
             {
-               
-                double dx = live_marker_x_;
-                double dy = live_marker_y_;
-
-                double dist_error = std::hypot(dx, dy);
+                // P-control
+                double dx = this->live_marker_x_;
+                double dy = this->live_marker_y_;
                 double angle_error = std::atan2(dy, dx);
-
-                // const double dist_th = 0.05;
                 const double angle_th = 0.02;
-
                 geometry_msgs::msg::Twist cmd;
-                // cmd.linear.x = std::clamp(0.4 * dist_error, 0.0, 0.2);
                 cmd.angular.z = 0.6 * angle_error;
-
                 cmd_vel_pub_->publish(cmd);
-                RCLCPP_INFO(get_logger(), "Navigation photo taken %d", angle_error);
 
                 if (std::abs(angle_error) < angle_th)
                 {
-                    RCLCPP_INFO(get_logger(), "align state , dopo if");
                     stop_robot();
                     save_img();
                     progress_ = 1.0;
+
                     send_feedback(progress_, "Aligned to " + marker_to);
                     this->state_ = AlignState::NAVIGATE;
-                    remove_first_marker_from_yaml();
-                    RCLCPP_INFO(get_logger(), "Navigation photo taken");
-                    marker_visible_ = false;
+                    remove_from_yaml();
+                    this->marker_visible_ = false;
                     finish(true, 1.0, "Completed");
                     return;
                 }
             }
         }
     }
-
+    // function to save image of marker
     void save_img()
     {
         try
+
         {
             cv_bridge::CvImagePtr cv_ptr = cv_bridge::toCvCopy(last_img_, sensor_msgs::image_encodings::BGR8);
             cv::circle(cv_ptr->image, cv::Point(cv_ptr->image.cols / 2, cv_ptr->image.rows / 2), 50, CV_RGB(0, 255, 0), 3);
-            // std::string dir_path = std::string(std::getenv("HOME")) + "/Desktop/Experimental/assignment2_ws/images";
+
+            // building in repository
             std::string ws_path = get_ws_path();
             std::string file_path = ws_path + "/images";
             if (!fs::exists(file_path))
             {
                 fs::create_directories(file_path);
             }
-
-            // std::ifstream infile(file_path);
-
-            // rcpputils::fs::create_directories(dir_path);
-            file_path = file_path + "/" + marker_target.name +"_" + std::to_string(marker_target.id) + ".png";
+            file_path = file_path + "/" + marker_target.name + "_" + std::to_string(marker_target.id) + ".png";
+            // saving img
             cv::imwrite(file_path, cv_ptr->image);
+
+            // publishing img on images_detected
             sensor_msgs::msg::Image::SharedPtr img_msg = cv_ptr->toImageMsg();
+            img_msg->header = last_img_->header;
             img_pub_->publish(*img_msg);
 
             RCLCPP_INFO(get_logger(), "Saved %s", file_path.c_str());
+            return;
         }
         catch (...)
         {
             RCLCPP_ERROR(get_logger(), "Save image error");
+            return;
         }
     }
-
+    // function to update the robot coordinates
     void odom_callback(const nav_msgs::msg::Odometry::SharedPtr msg)
     {
         current_x_ = msg->pose.pose.position.x;
         current_y_ = msg->pose.pose.position.y;
     }
-
+    // on activate for plansys
     rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface::CallbackReturn
     on_activate(const rclcpp_lifecycle::State &previous_state) override
     {
@@ -438,7 +476,7 @@ private:
         marker_target.name = "";
         return plansys2::ActionExecutorClient::on_activate(previous_state);
     }
-
+    // function to make robot stop
     void stop_robot()
     {
         geometry_msgs::msg::Twist cmd;
@@ -447,29 +485,34 @@ private:
 
     float progress_;
     bool goal_sent_, nav2_done_, nav2_success_;
+    bool marker_visible_ = false;
+    double live_marker_y_ = 0.0;
+    double live_marker_x_ = 0.0;
     double current_x_, current_y_;
     double start_x_ = 0.0, start_y_ = 0.0;
+
     MarkerData marker_target;
+    std::vector<MarkerData> targets_list;
     rclcpp::Subscription<nav_msgs::msg::Odometry>::SharedPtr odom_sub_;
-    rclcpp_action::Client<nav2_msgs::action::NavigateToPose>::SharedPtr nav2_client_;
     rclcpp::Subscription<sensor_msgs::msg::Image>::SharedPtr img_sub_;
+    rclcpp::Subscription<aruco_opencv_msgs::msg::ArucoDetection>::SharedPtr aruco_sub_;
+
     rclcpp::Publisher<sensor_msgs::msg::Image>::SharedPtr img_pub_;
+    rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr cmd_vel_pub_;
+
     sensor_msgs::msg::Image::SharedPtr last_img_;
     AlignState state_ = AlignState::NAVIGATE;
-    rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr cmd_vel_pub_;
-    rclcpp::Subscription<aruco_opencv_msgs::msg::ArucoDetection>::SharedPtr aruco_sub_;
     std::shared_ptr<tf2_ros::Buffer> tf_buffer_;
     std::shared_ptr<tf2_ros::TransformListener> tf_listener_;
-    double live_marker_x_ = 0.0;
-    double live_marker_y_ = 0.0;
-    bool marker_visible_ = false;
+
+    rclcpp_action::Client<nav2_msgs::action::NavigateToPose>::SharedPtr nav2_client_;
 };
 
 int main(int argc, char **argv)
 {
     rclcpp::init(argc, argv);
     auto node = std::make_shared<MoveToPhotograph>();
-    node->set_parameter(rclcpp::Parameter("action_name", "move_to_photograph_mode"));
+    node->set_parameter(rclcpp::Parameter("action_name", "move_to_photograph"));
     node->trigger_transition(lifecycle_msgs::msg::Transition::TRANSITION_CONFIGURE);
     node->trigger_transition(lifecycle_msgs::msg::Transition::TRANSITION_ACTIVATE);
     rclcpp::spin(node->get_node_base_interface());
